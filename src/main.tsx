7bffd7d39f2c65b843eaeaf4dc7eae35f061de45
Leonardo Araujo Sarmento <leosarmento@ucl.br>
2024-02-04 21:20:12 -0300

    Setup for new project

---
+++ src/main.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from '@services/state/store.ts';
+
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>,
+  );
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+}
